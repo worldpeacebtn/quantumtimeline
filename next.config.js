b3ac9051da3abe9f7018ba737a6e64a2146519eb
worldpeacebtn <worldpeacebtn@gmail.com>
2025-08-10 13:27:33 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: '/quantumtimeline',
+  assetPrefix: '/quantumtimeline/',
+};
 
 module.exports = nextConfig;
